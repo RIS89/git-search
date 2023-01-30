0e15ad39ebc286724d56eaab14980631848a8472
RIS89 <ilyarundin89@gmail.com>
2023-01-30 16:31:01 +0300

    доустановил плагины

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWbpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/app.js",
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "build"),
+    filename: "main-[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
+    new HtmlWbpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: "site.css",
+      filename: "site-[contenthash].css",
     }),
   ],
+  mode: "development",
   module: {
     rules: [
       {
